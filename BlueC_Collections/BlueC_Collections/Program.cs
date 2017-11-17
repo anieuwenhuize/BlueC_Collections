2d09457434a1667a0911655d37ce63eab913e33f
Andries <nieu0082@hz.nl>
2017-11-17 09:38:10 +0100

    Project aangemaakt

---
+++ BlueC_Collections/BlueC_Collections/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BlueC_Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zet 'm op!");
+        }
+    }
+}
---
+++ BlueC_Collections/BlueC_Collections/TryOutCollections.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlueC_Collections
+{
+    class TryOutCollections
+    {
+        /// <summary>
+        /// een I?<int> accepteert en deze vult met 200 even getallen;
+        /// </summary>
+        /// <returns></returns>
+        public ? AddNumbers(? collectie)
+        {
+
+        }
+
+        /// <summary>
+        /// accepteert en de getallen 6 en 8 verwijderd;
+        /// </summary>
+        /// <returns></returns>
+        public ? Remove6And8(? collectie)
+        {
+
+        }
+
+        /// <summary>
+        /// accepteert en deze doorloopt en alle overgebleven getallen in de console print.
+        /// </summary>
+        /// <returns></returns>
+        public ? PrintAllNumbers(? collectie)
+        {
+
+        }
+    }
+}
